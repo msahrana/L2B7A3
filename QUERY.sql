679cb86686cdb318db540bb977edc7ef0bd0f463
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-06-14 09:00:52 +0600

    all query work in seccessfully

--- QUERY.sql
+++ QUERY.sql
@@ -60,6 +60,60 @@ CREATE TABLE Bookings (
 
 
 -- =========================================================================
+-- CREATE USERS TABLE
+-- =========================================================================
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    role VARCHAR(20) NOT NULL
+        CHECK (role IN ('Football Fan', 'Ticket Manager')),
+    phone_number VARCHAR(20)
+);
+
+-- =========================================================================
+-- CREATE MATCHES TABLE
+-- =========================================================================
+CREATE TABLE matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(255) NOT NULL,
+    tournament_category VARCHAR(255) NOT NULL,
+    base_ticket_price DECIMAL(10,2) NOT NULL
+        CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(20) NOT NULL
+        CHECK (
+            match_status IN
+            ('Available', 'Selling Fast', 'Sold Out', 'Postponed')
+        )
+);
+
+-- =========================================================================
+-- CREATE BOOKINGS TABLE
+-- =========================================================================
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20)
+        CHECK (
+            payment_status IN
+            ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+        ),
+    total_cost DECIMAL(10,2) NOT NULL
+        CHECK (total_cost >= 0),
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+
+    CONSTRAINT fk_match
+        FOREIGN KEY (match_id)
+        REFERENCES matches(match_id)
+);
+
+
+-- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
 INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
@@ -87,3 +141,81 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+-- =========================================================================
+-- QUERY-1:
+-- =========================================================================
+select match_id,
+       fixture,
+       (base_ticket_price)
+from matches
+where tournament_category = 'Champions League'
+  AND match_status = 'Available';
+
+-- =========================================================================
+-- QUERY-2:
+-- =========================================================================
+select user_id,
+       full_name,
+       email
+from users
+where full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
+
+-- =========================================================================
+-- QUERY-3:
+-- =========================================================================
+select booking_id,
+       user_id,
+       match_id,
+       COALESCE(payment_status, 'Action Required')
+       as systematic_status
+from bookings
+where payment_status IS NULL;
+
+-- =========================================================================
+-- QUERY-4:
+-- =========================================================================
+select b.booking_id,
+       u.full_name,
+       m.fixture,
+       b.total_cost
+from bookings b
+inner join users u
+    ON b.user_id = u.user_id
+inner join matches m
+    ON b.match_id = m.match_id;
+
+-- =========================================================================
+-- QUERY-5:
+-- =========================================================================
+select u.user_id,
+       u.full_name,
+       b.booking_id
+from users u
+left join bookings b
+    ON u.user_id = b.user_id
+order by u.user_id;
+
+-- =========================================================================
+-- QUERY-6:
+-- =========================================================================
+select booking_id,
+       match_id,
+       total_cost
+from bookings
+where total_cost >
+(
+    select avg(total_cost)
+    from bookings
+);
+
+-- =========================================================================
+-- QUERY-7:
+-- =========================================================================
+select match_id,
+       fixture,
+       base_ticket_price
+from matches
+order by base_ticket_price DESC limit 2 offset 1;
+
